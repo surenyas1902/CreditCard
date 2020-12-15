282c5afc72f2766f9dbc816bebbbaa9d1a164a96
Surendiran S <44779174+surenyas1902@users.noreply.github.com>
2020-12-15 19:43:10 +0530

    learning Form Controls and Validation.

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -13,7 +13,9 @@ export class CardFormComponent implements OnInit {
     ])
   });
 
-  constructor() { }
+  constructor() {
+    console.log(this.cardForm.controls.name)
+   }
 
   ngOnInit(): void {
   }
